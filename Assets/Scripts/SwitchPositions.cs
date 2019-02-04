5baf79bac5cc67cae443d7054f7be4c14175b861
Rebecca Medina (RIT Student) <rcm3517@ad.rit.edu>
2019-02-04 12:50:14 -0500

    Fix fuse button

---
+++ Assets/Scripts/SwitchPositions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPositions : MonoBehaviour {
+
+    public Vector3[] positions;
+    public int currPosIndex = 0;
+    public float t;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = Vector3.Lerp(positions[currPosIndex%positions.Length], positions[(currPosIndex+1)%positions.Length], t);
+        t += 0.01f;
+	}
+
+    public void SwitchPosition()
+    {
+        currPosIndex++;
+        t = 0;
+    }
+}
